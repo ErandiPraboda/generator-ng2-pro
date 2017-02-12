63e2a1f7a4ce559842c0c0e26987ff9a2dfed667
Erandi1234 <erandipraboda@gmail.com>
2017-02-12 11:35:37 +0530

    adding webpack.config and changing folder structure

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,40 +11,7 @@ module.exports = Generator.extend({
       'Welcome to the super-duper ' + chalk.red('generator-ng2-pro') + ' generator!'
     ));
 
-    var prompts = [{
-     type: 'checkbox',
-     name: 'mainframeworks',
-     message:'Would you like AngularJS or JQuery ?',
-     choices: [{
-       name: 'Angular',
-       value: 'includeAngular',
-       checked: true
-      }, {
-       name: 'JQuery',
-       value: 'includeJQuery',
-       checked: true
-      }]
-    },
-    {
-     type: 'checkbox',
-     name: 'features',
-     message:'What more front-end frameworks would you like ?',
-     choices: [{
-       name: 'Sass',
-       value: 'includeSass',
-       checked: true
-     }, 
-     {
-      name: 'Bootstrap',
-      value: 'includeBootstrap',
-      checked: true
-     }, 
-     {
-      name: 'Modernizr',
-      value: 'includeModernizr',
-      checked: true
-     }]
-     },
+    var prompts = [
      {
       type    : 'input',
       name    : 'name',
@@ -104,16 +71,6 @@ module.exports = Generator.extend({
         );
 
         this.fs.copyTpl(
-            this.templatePath('karma.conf.js'),
-            this.destinationPath('karma.conf.js')
-        );
-
-        this.fs.copyTpl(
-            this.templatePath('karma-shim.js'),
-            this.destinationPath('karma-shim.js')
-        );
-
-        this.fs.copyTpl(
             this.templatePath('protractor.conf.js'),
             this.destinationPath('protractor.conf.js')
         );
@@ -132,61 +89,42 @@ module.exports = Generator.extend({
             this.templatePath('typings.json'),
             this.destinationPath('typings.json')
         );
-
+        this.fs.copyTpl(
+            this.templatePath('src/*'),
+            this.destinationPath('src/')
+        );
+        this.fs.copyTpl(
+            this.templatePath('src/app/*'),
+            this.destinationPath('src/app')
+        );
+        this.fs.copyTpl(
+            this.templatePath('config/*'),
+            this.destinationPath('config/')
+        );
+        this.fs.copyTpl(
+            this.templatePath('public/css/*'),
+            this.destinationPath('public/css')
+        );
+        this.fs.copyTpl(
+            this.templatePath('public/images/*'),
+            this.destinationPath('public/images')
+        );
         this.fs.copyTpl(
             this.templatePath('webpack.config.js'),
             this.destinationPath('webpack.config.js')
         );
-   },
-  bower: function()
-  {
-  var bowerJson = {
-                name: this.name, 
-                license: 'MIT',
-                dependencies: {}  
-            };
-            bowerJson.dependencies['angular'] = '~1.4.6';
-            bowerJson.dependencies['angular-bootstrap'] = '~0.13.4';
-            bowerJson.dependencies['angular-ui-router'] = '~0.2.15';
-            bowerJson.dependencies['bootstrap-css-only'] = '~3.3.5';
-        
-  if (this.includeBootstrap) {
-        var bs = 'bootstrap' + (this.includeSass ? '-sass' : '');
-        bowerJson.dependencies['bs'] = '~3.3.1';
-  }
-  if (this.includeModernizr) {
-        bowerJson.dependencies['modernizr'] = '~2.8.1';
-  }
-  if (this.includeAngular) {
-        bower.dependencies['angular'] = '~1.3.15';
-  }  
-  if (this.includeJQuery) {
-        bower.dependencies['jquery'] = '~2.1.1';
-  }
-       this.fs.writeJSON('bower.json', bowerJson);
-
-        this.fs.copy(
-        this.templatePath('bowerrc'),
-        this.destinationPath('.bowerrc')
-      );
-  },
+},
  
- application :function()
- {
-  var componentJson = {};
-  this.fs.writeJSON('app/'+this.name+'/'+this.name+'.component.ts',componentJson);
- 
-  var componentJson = {};
-  this.fs.writeJSON('app/'+this.name+'/'+this.name+'.service.ts',componentJson);
-
-  var componentJson = {};
-  this.fs.writeJSON('app/'+this.name+'/'+this.name+'.ts',componentJson);
- },
- 
-
 
   install: function () {
     this.installDependencies({
+        yarn :true,
+        npm : false,
+        bower : false,
+        callback: function ()
+        {
+        console.log('Everything is ready!');
+        }
 
     });
   }
---
+++ generators/app/templates/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+export class Hero {
+  id: number;
+  name: string;
+}
+
+@Component({
+  
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  
+})
+
+export class AppComponent {
+    
+}
